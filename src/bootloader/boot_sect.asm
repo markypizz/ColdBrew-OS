1e5bda18ecf37676c69bcbbb865475c3353347b6
Mark Pizzutillo <markypizz@gmail.com>
2019-07-22 23:54:21 -0500

    Working switch to protected mode. Long mode next?

--- src/bootloader/boot_sect.asm
+++ src/bootloader/boot_sect.asm
@@ -1,72 +1,67 @@
-[org 0x7C00] ;boot sector location
-KERNEL_ADDRESS_OFFSET equ 0x1000
-
-[bits 16]
-
-; hold onto boot drive value
-mov [BOOT_DRIVE], dl 
-
-; set up stack
-mov bp, 0x9000
-mov sp, bp ; empty stack
+[org 0x7c00]
+[BITS 16]
 
 start:
-    ;mov bx, LAUNCH_TEXT
-    ;call print_text
-    ;call newline
+	cli ; clear interrupts
+	lgdt [gdt_descriptor] ; load GDT
 
-    ; Start loading of DISK
-    ;mov bx, DISK_LOAD_TEXT
-    ;;call print_text
-    ;call newline
+	mov eax, cr0
+	or eax,0x1
+	mov cr0, eax ; kick off transition
+	jmp CODE_SEG:main_protected ; long jump to proected mode code
 
-    mov bx, 0x9000
-    mov dh, 2 ; 2 sectors
-    call load_disk
+gdt_start:
+	dq 0x0          ; null bytes
+gdt_code:
+	dw 0xFFFF       ; segment limit
+	dw 0x0          ; segment base addr
+	db 0x0          ; segment base addr
+	db 10011010b    ; Readable segment, code segment, present flag
+	db 11001111b    ; Segment limit, 32 bit size, 4 GB
+	db 0x0          ; segment base addr
+gdt_data:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
 
-    ;mov bx, DISK_LOAD_SUCCESS
-    ;call print_text
-    ;call newline
+gdt_end:
 
-    ; switch to LONG mode
-    jmp enter_long_mode
+gdt_descriptor:
+	dw gdt_end - gdt_start
+	dd gdt_start
 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
+[BITS 32]
+main_protected:
+	mov ax, DATA_SEG ; set up regs
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 
-;%include "src/bootloader/include/boot_sect_print.asm"
-%include "src/bootloader/include/boot_sect_disk_load.asm"
-%include "src/bootloader/include/boot_sect_long_mode.asm"
+	mov esi, hello ; string
+	mov ebx, 0xb8000 ; video memory
+    mov esp, 0x9000 ; stack pointer
 
-[bits 64]
-LONG_MODE_MAIN:
+printloop:
+	lodsb
+	or al, al ; check for terminating null byte
+	jz halt ; done printing
+	or eax, 0x0200 ; text color
+	mov word [ebx], ax
+	add ebx, 2
+	jmp printloop
+halt:
+	cli
+	hlt
+hello: db "Welcome to CBOS", 0
 
-    cli                           ; Clear the interrupt flag.
-    mov ax, GDT64.Data            ; Set the A-register to the data descriptor.
-    mov ds, ax                    ; Set the data segment to the A-register.
-    mov es, ax                    ; Set the extra segment to the A-register.
-    mov fs, ax                    ; Set the F-segment to the A-register.
-    mov gs, ax                    ; Set the G-segment to the A-register.
-    mov ss, ax                    ; Set the stack segment to the A-register.
-    mov edi, 0xB8000              ; Set the destination index to 0xB8000.
-    mov rax, 0x1F201F201F201F20   ; Set the A-register to 0x1F201F201F201F20.
-    mov ecx, 500                  ; Set the C-register to 500.
-    rep stosq                     ; Clear the screen.
-
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
-    hlt
-    
-BOOT_DRIVE: db 0
-;LAUNCH_TEXT: db 'Welcome to ColdBrew OS', 0 ; c-style end with null byte
-;DISK_LOAD_TEXT: db 'Starting load from DISK...', 0
-;DISK_LOAD_SUCCESS: db 'Loaded DISK successfully.',0
-LONG_MODE_STR: db 'Long Mode Enabled.',0
-
+; boot sector signature
 times 510-($-$$) db 0
-dw 0xaa55 ; bootsector indicator
-
-; sector 2
-times 256 dw 0xaabb
-
-; sector 3
-times 256 dw 0xccdd
+dw 0xaa55
